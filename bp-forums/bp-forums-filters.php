4626760744dafb1ceb1b4ba703297a80f37f3916
apeatling <apeatling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-06-24 21:00:29 +0000

    tagging 1.0.2
    git-svn-id: https://plugins.svn.wordpress.org/buddypress/tags/1.0.2@129175 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bp-forums/bp-forums-filters.php
@@ -0,0 +1,38 @@
+<?php
+
+/* BuddyPress filters */
+add_filter( 'bp_get_the_topic_latest_post_excerpt', 'bp_create_excerpt' );
+
+/* Apply WordPress defined filters */
+add_filter( 'bp_get_the_topic_title', 'wptexturize' );
+add_filter( 'bp_get_the_topic_poster_name', 'wptexturize' );
+add_filter( 'bp_get_the_topic_last_poster_name', 'wptexturize' );
+add_filter( 'bp_get_the_topic_post_content', 'wptexturize' );
+add_filter( 'bp_get_the_topic_post_poster_name', 'wptexturize' );
+
+add_filter( 'bp_get_the_topic_title', 'convert_smilies' );
+add_filter( 'bp_get_the_topic_latest_post_excerpt', 'convert_smilies' );
+add_filter( 'bp_get_the_topic_post_content', 'convert_smilies' );
+
+add_filter( 'bp_get_the_topic_title', 'convert_chars' );
+add_filter( 'bp_get_the_topic_latest_post_excerpt', 'convert_chars' );
+add_filter( 'bp_get_the_topic_post_content', 'convert_chars' );
+
+add_filter( 'bp_get_the_topic_post_content', 'wpautop' );
+add_filter( 'bp_get_the_topic_latest_post_excerpt', 'wpautop' );
+
+add_filter( 'bp_get_the_topic_post_content', 'stripslashes_deep' );
+add_filter( 'bp_get_the_topic_title', 'stripslashes_deep' );
+add_filter( 'bp_get_the_topic_latest_post_excerpt', 'stripslashes_deep' );
+
+add_filter( 'bp_get_the_topic_post_content', 'make_clickable' );
+
+function bp_forums_add_allowed_tags( $allowedtags ) {
+	$allowedtags['p'] = array();
+	$allowedtags['br'] = array();
+	
+	return $allowedtags;
+}
+add_filter( 'edit_allowedtags', 'bp_forums_add_allowed_tags' );
+
+?>
