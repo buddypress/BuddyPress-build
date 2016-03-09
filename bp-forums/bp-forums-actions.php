8cca2c7d6832f39e1f27c942c7100ff1b966bd0e
imath <imath@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-03-09 23:23:33 +0000

    Create 2.5.1 tag
    git-svn-id: https://plugins.svn.wordpress.org/buddypress/tags/2.5.1@1367946 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bp-forums/bp-forums-actions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * BuddyPress Forums Actions.
+ *
+ * @package BuddyPress
+ * @subpackage ForumsActions
+ * @since 1.5.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
