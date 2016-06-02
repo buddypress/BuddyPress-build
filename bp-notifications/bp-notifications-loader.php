07c20e60290db56e03881f8bf73ce81367ff74ce
dcavins <dcavins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-06-02 02:48:38 +0000

    Create tag 2.6.0-beta1
    git-svn-id: https://plugins.svn.wordpress.org/buddypress/tags/2.6.0-beta1@1428775 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bp-notifications/bp-notifications-loader.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * BuddyPress Member Notifications Loader.
+ *
+ * Initializes the Notifications component.
+ *
+ * @package BuddyPress
+ * @subpackage NotificationsLoader
+ * @since 1.9.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( ! buddypress()->do_autoload ) {
+	require dirname( __FILE__ ) . '/classes/class-bp-notifications-component.php';
+}
+
+/**
+ * Bootstrap the Notifications component.
+ *
+ * @since 1.9.0
+ */
+function bp_setup_notifications() {
+	buddypress()->notifications = new BP_Notifications_Component();
+}
+add_action( 'bp_setup_components', 'bp_setup_notifications', 6 );
