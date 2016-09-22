0cf17b70696fc53341a8b8eb101ca2e74c52a376
mercime <mercime@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-09-22 19:10:53 +0000

    Create tag 2.7.0-beta1.
    git-svn-id: https://plugins.svn.wordpress.org/buddypress/tags/2.7.0-beta1@1500562 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bp-friends/bp-friends-classes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * BuddyPress Friends Classes.
+ *
+ * @package BuddyPress
+ * @subpackage FriendsClasses
+ * @since 1.0.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+require dirname( __FILE__ ) . '/classes/class-bp-friends-friendship.php';
