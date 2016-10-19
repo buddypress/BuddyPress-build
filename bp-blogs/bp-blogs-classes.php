b6a8094d7cf034a888b2ec2afe62ff510393256e
mercime <mercime@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-10-19 18:51:30 +0000

    Create tag 2.7.0.
    git-svn-id: https://plugins.svn.wordpress.org/buddypress/tags/2.7.0@1518053 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bp-blogs/bp-blogs-classes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * BuddyPress Blogs Classes.
+ *
+ * @package BuddyPress
+ * @subpackage BlogsClasses
+ * @since 1.0.0
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+require dirname( __FILE__ ) . '/classes/class-bp-blogs-blog.php';
